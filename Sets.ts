ec78d13327e02e25158914f81ab837c5d9a13e30
Charlie <howesybusiness@gmail.com>
2020-12-17 01:36:39 +0000

    Added JSDoc comments to "determineSubsets" and "determineCardinality"

--- Sets.ts
+++ Sets.ts
@@ -1,9 +1,20 @@
+/**
+ * Function to determine amount of subsets from specified subset.
+ * @param specifiedSet The set you wish to determine the amount of subsets for.
+ * @param excludeEmptySet Optional boolean to include the empty set when determining the amount of subsets.
+ */
+
 function determineSubsets<T>(specifiedSet: Set<T>, excludeEmptySet = false): number {
     const calculatedSubsets: number = Math.pow(2, specifiedSet.size);
     if (excludeEmptySet)
         return calculatedSubsets - 1;
     return calculatedSubsets;
 }
+
+/**
+ * Function to determine the cardinality of a specified set.
+ * @param specifiedSet The set you wish to determine the cardinality of.
+ */
 
 function determineCardinality<T>(specifiedSet: Set<T>): number {
     return specifiedSet.size;
