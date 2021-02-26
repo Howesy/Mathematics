2ebe6fe9337ad7a6cd516856e9b035d17237c7d1
Charlie <howesybusiness@gmail.com>
2021-02-26 22:30:50 +0000

    Added "calculateSFIS"

---
+++ calculateSFIS.ts
@@ -0,0 +1,7 @@
+//Calculate how many squares (x * y) fit into another square (a * b)
+function calculateSFIS(x: number, y: number, a: number, b:number) : number {
+    const calculatedX = a / x;
+    const calculatedY: number = b / y;
+    const calculation = calculatedX * calculatedY;
+    return calculation;
+}
