bfaa78a234a6c4de722dfa7c1177b2300aa34630
Charlie <howesybusiness@gmail.com>
2021-02-26 22:24:49 +0000

    Removed unnecessary type conversion.

--- complements.cpp
+++ complements.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 
 int32_t calculateNinesComplement(int32_t x) {
-	int32_t calculatedDigits = reinterpret_cast<int32_t>(log(x) + 1);
+	int32_t calculatedDigits = log(x) + 1;
 	int32_t calculation = (pow(10, calculatedDigits) + 1) - x;
 	return calculation;
 }
