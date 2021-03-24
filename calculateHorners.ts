9d2a4ad422336b5e5d2cfbe0c018362bda9edebc
Charlie <howesybusiness@gmail.com>
2021-03-24 12:16:40 +0000

    Added "calculateHorners.ts"

---
+++ calculateHorners.ts
@@ -0,0 +1,26 @@
+function calculateHorners(binaryValue: number): number {
+    const calculatedConsecutiveValues: Array<number> = [];
+    const binaryStringArray = binaryValue.toString().split("");
+    const [initialValue] = binaryStringArray;
+    let parsedInitialValue = parseInt(initialValue);
+    const preparedBinaryValue = binaryStringArray.slice(1, binaryStringArray.length);
+    calculatedConsecutiveValues.push(parsedInitialValue);
+    preparedBinaryValue.forEach(function(bit) {
+        parsedInitialValue *= 2;
+        parsedInitialValue += parseInt(bit);
+        calculatedConsecutiveValues.push(parsedInitialValue);
+    });
+
+    console.log(`Calculated Consectuive Values using the Horners Method for ${binaryValue}: \n${calculatedConsecutiveValues.join(", ")}\n`);
+
+    return parsedInitialValue;
+}
+
+const hornersValue: number = calculateHorners(110110);
+console.log(hornersValue);
+
+//Horners Algorithm Logic.
+//-> Take down first value.
+//-> Add the binary value to the value.
+//-> Multiply value by 2 and assign value to itself.
+//-> Add value to array.
